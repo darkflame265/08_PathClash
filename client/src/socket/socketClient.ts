5a7c2e0e1fdd67c4e0b8aa20bb3d04c631abb65d
darkflame265 <ter2490@naver.com>
2026-02-28 12:39:02 +0900

    "use env server url"

--- client/src/socket/socketClient.ts
+++ client/src/socket/socketClient.ts
@@ -1,6 +1,6 @@
-import { io, Socket } from 'socket.io-client';
+import { io, Socket } from "socket.io-client";
 
-const SERVER_URL = 'http://localhost:3001';
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 let socket: Socket | null = null;
 
