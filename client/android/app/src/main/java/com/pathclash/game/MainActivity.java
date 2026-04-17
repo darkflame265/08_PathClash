bb9ac0295030acd96358408a1273b174c7e0b38a
darkflame265 <ter2490@naver.com>
2026-04-17 13:51:01 +0900

    모바일에서는 가로모드 안 되도록 ㅊ ㅏ단.

--- client/android/app/src/main/java/com/pathclash/game/MainActivity.java
+++ client/android/app/src/main/java/com/pathclash/game/MainActivity.java
@@ -1,5 +1,7 @@
 package com.pathclash.game;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -7,8 +9,11 @@ import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private static final int TABLET_SMALLEST_WIDTH_DP = 600;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        applyDeviceOrientationPolicy();
         super.onCreate(savedInstanceState);
         applyImmersiveMode();
     }
@@ -32,5 +37,16 @@ public class MainActivity extends BridgeActivity {
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             );
         }
+    }
+
+    private void applyDeviceOrientationPolicy() {
+        Configuration configuration = getResources().getConfiguration();
+        boolean isTablet = configuration.smallestScreenWidthDp >= TABLET_SMALLEST_WIDTH_DP;
+
+        setRequestedOrientation(
+            isTablet
+                ? ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+                : ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        );
     }
 }
