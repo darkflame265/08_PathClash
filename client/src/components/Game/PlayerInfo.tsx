5fd7f89863aea58bdbfa8e9177789ee7b876b8ea
darkflame265 <ter2490@naver.com>
2026-05-05 23:48:40 +0900

    인게임 상대 프로필창에서 상대 승리,패배 횟수 표기 삭제.
    수정했어요. 인게임 상대 닉네임 클릭 시 뜨는 프로필에서 승리/패배 횟수 행을 제거했고, 승률만 표시됩니다.
    
    수정 파일:
    
    PlayerInfo.tsx

--- client/src/components/Game/PlayerInfo.tsx
+++ client/src/components/Game/PlayerInfo.tsx
@@ -50,14 +50,6 @@ export function PlayerInfo({ player, isMe }: Props) {
             <span>{player.nickname}</span>
           </div>
           <div className="profile-row">
-            <span>{t.profileRecord}</span>
-            <span>
-              {player.stats.wins}
-              {t.profileWins} {player.stats.losses}
-              {t.profileLosses}
-            </span>
-          </div>
-          <div className="profile-row">
             <span>{t.profileWinRate}</span>
             <span>{winRate}%</span>
           </div>
